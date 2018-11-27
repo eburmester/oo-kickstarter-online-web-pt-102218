b9b8f457f54adf6f75a4eca2b4e95f57a3344b5d
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-27 18:22:35 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,8 @@ class Backer
   
   def back_project(project)
     @backed_projects << project 
-    project.add_backer(self) unless project.backers.include?(self)
+    
+    backer.back_project(self) unless backer.back_projects.include?(self)
   end
   
 end
